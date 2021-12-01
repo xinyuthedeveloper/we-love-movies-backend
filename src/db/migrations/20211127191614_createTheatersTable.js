8d18dc9283f5ff2a58b378a7b529df5802cb82f0
Xin Yu <wilsonyu0626@gmail.com>
2021-12-01 15:08:00 -0800

    Add timestamps for all the tables

--- src/db/migrations/20211127191614_createTheatersTable.js
+++ src/db/migrations/20211127191614_createTheatersTable.js
@@ -8,6 +8,8 @@ exports.up = function(knex) {
         table.string("city");
         table.string("state");
         table.string("zip");
+        table.timestamp("created_at").defaultTo(knex.fn.now());
+        table.timestamp("updated_at").defaultTo(knex.fn.now());
     })
 };
 
